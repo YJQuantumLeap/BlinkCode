1abaabd37846fcc1c7bf70a1e0b451c563cc90af
Huangyajun <1754552172@qq.com>
2024-07-01 15:01:53 +0800

    blink

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,4 @@
-conda create -n MLLM-code python=3.10.14
-conda activate MLLM-code
+conda create -n blinkcode python=3.10.14
+conda activate blinkcode
 conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch
 pip install -r requirements.txt
